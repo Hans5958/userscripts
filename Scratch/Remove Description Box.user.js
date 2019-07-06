f9d3bd109d48f0336630d5d7110f53b33f72bc1c
Hans5958 <email5958.1@gmail.com>
2019-07-06 23:18:03 +0700

    Update "Remove Description Box"

--- Scratch/Remove Description Box.user.js
+++ Scratch/Remove Description Box.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Description Box on Scratch
 // @namespace    scratch.mit.edu/users/Hans5958
-// @version      3
+// @version      4
 // @description  Remove the instruction/notes box on Scratch
 // @copyright    Hans5958
 // @license      MIT
@@ -11,6 +11,7 @@
 
 window.onload = function() {
     var r = document.getElementsByClassName("project-textlabel")
-    r[0].innerHTML += " <a class='remove' onclick='var e=e=>document.getElementsByClassName(e),r=e(\"project-textlabel\"),d=e(\"description-block\");d[1].remove(),e(\"remove\")[0].hidden=!0;'>❌</a>"
-    r[1].innerHTML += " <a class='remove' onclick='var e=e=>document.getElementsByClassName(e),r=e(\"project-textlabel\"),d=e(\"description-block\");d[2].remove(),e(\"remove\")[0].hidden=!0;'>❌</a>"
+    var x = y => " <a class='remove' onclick='var e=e=>document.getElementsByClassName(e),r=e(\"project-textlabel\"),d=e(\"description-block\");d[" + y + "].remove(),e(\"remove\")[0].hidden=!0;'>❌</a>"
+    r[0].innerHTML += x(0)
+    r[1].innerHTML += x(1)
 }
