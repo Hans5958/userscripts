81fb0ab32028cfc36411cc42ac9b83cdec8e9c63
Hans5958 <hans5958@outlook.com>
2023-01-07 19:19:57 +0700

    Akinator Progression Visualizer: Update sidebar element selector

--- Other Scripts/Akinator Progression Visualizer.user.js
+++ Other Scripts/Akinator Progression Visualizer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Akinator Progression Visualizer
 // @namespace    Hans5958
-// @version      1.0.0
+// @version      1.0.1
 // @description  Adds visualization of Akinator's progression (aka. certainity, probability). Inspired by the moving vertical background of the app's old version.
 // @author       Hans5958
 // @match        https://*.akinator.com/game
@@ -11,7 +11,7 @@
 
 let lastData = ""
 let sliderElement = document.createElement('div')
-let sidebarElement = document.querySelector('div.col-md-2:nth-child(3)')
+let sidebarElement = document.querySelector('div.col-md-2:nth-child(4)')
 
 sliderElement.innerHTML = `<div style="position: absolute;height: 100%;"><div style="height: 100%;display: flex;flex-direction: row-reverse;"><div style="width: 2rem;border: black solid 0.125rem;z-index: 200;display: flex;flex-direction: column-reverse;height: 100%;z-index: 10;"><div id="progression-slider" style="background-color: black; display: flex; height: 0%;"></div><div id="progression-text" style="position: relative; text-align: right; right: 11rem; margin-bottom: -14px; width: 10rem;bottom: 0%;">0%</div></div></div></div>`
 sliderElement = sliderElement.firstChild
